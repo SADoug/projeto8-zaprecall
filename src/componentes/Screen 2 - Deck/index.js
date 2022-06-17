3f7149fc1b9f7c444505fd73e343a915abad5c83
SADoug <douglastec2@gmail.com>
2022-06-16 23:50:51 -0300

    finalized

--- src/componentes/Screen 2 - Deck/index.js
+++ src/componentes/Screen 2 - Deck/index.js
@@ -3,6 +3,7 @@ import "./style.css"
 import LogoPequeno from "./../../assets/img/logo-pequeno.png";
 import data from "./../../data"
 import Flashcard from "./../Flashcard";
+import Icone from "../Icone";
 
 /*FUNÇÃO QUE MOSTRA AS QUESTOES NA TELA*/
 function Deck() {
@@ -17,16 +18,52 @@ function Deck() {
         } else {
             return questions.map((question, indice) => {
                 const { frente, verso } = question;
-                return <Flashcard 
-                key={frente + indice}
-                indice={indice + 1}
-                frente={frente}
-                verso={verso}
-                aoFinalizar={anwser => setAnwsers([...anwsers, anwser])}
-              />
+                return <Flashcard
+                    key={frente + indice}
+                    indice={indice + 1}
+                    frente={frente}
+                    verso={verso}
+                    aoFinalizar={anwser => setAnwsers([...anwsers, anwser])}
+                />
             })
         }
     }
+
+
+    function montarFooter() {
+        let resultado = <></>;
+
+        if (anwsers.length === questions.length && questions.length > 0) {
+            if (!anwsers.includes("erro")) {
+                resultado = (
+                    <div className="resultado">
+                        <span><Icone icone="festinha" /> Parabéns!</span>
+                        <p>Você não esqueceu de nenhum flashcard!</p>
+                    </div>
+                )
+            } else {
+                resultado = (
+                    <div className="resultado">
+                        <span><Icone icone="triste" /> Putz!</span>
+                        <p>Ainda faltam alguns...Mas não desanime!</p>
+                    </div>
+                )
+            }
+        }
+
+
+        return (
+            <>
+                {resultado}
+                <p>{anwsers.length}/{questions.length} concluídos</p>
+                {anwsers.map(resposta => <Icone icone={resposta} />)}
+            </>
+        )
+    }
+
+
+    const footer = montarFooter();
+
 
     const flashcards = makeupFlashcards();
 
@@ -40,7 +77,7 @@ function Deck() {
                 {flashcards}
             </main>
             <footer>
-                <p>0/1 Concluídos!</p>
+                {footer}
             </footer>
         </div>
     )
